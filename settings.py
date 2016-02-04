989b30bf35a3cb6d147fc7506b9f5f8be63343fb
tfiers <tomas.fiers@gmail.com>
2016-02-04 03:54:45 +0100

    Convert tabs to spaces.

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ Provides access accross modules to app settings.
 
 Usage:
 
-	from settings import settings
-	settings['key']
+    from settings import settings
+    settings['key']
 
 This includes access to secret settings, which should be saved in the 
 directory 'secrets', in a gitignored file called 'secret_settings.json'. 
@@ -19,24 +19,24 @@ import json
 
 settings = {}
 with open('secrets/secret_settings.json') as f:
-	settings.update(json.load(f))
+    settings.update(json.load(f))
 
 # -------------------------------------------------------------------
 
 def generate_dummy_secret_settings():
-	'''
-	Creates a file that shows which key-value pairs should be in the 
-	real 'secrets/secret_settings.json' file. The keys are preserved 
-	and the secret values are replaced by a default instantiation 
-	of their original datatype ('0' for ints, '{}' for dicts, etc.). 
-	'''
-	with open('secrets/secret_settings.json') as f:
-		secrets = json.load(f)
-	for key in secrets:
-		t = type(secrets[key])
-		secrets[key] = t.__new__(t)
-	with open('secrets/dummy_secret_settings.json', 'wb') as f:
-		json.dump(secrets, f, indent=4)
+    '''
+    Creates a file that shows which key-value pairs should be in the 
+    real 'secrets/secret_settings.json' file. The keys are preserved 
+    and the secret values are replaced by a default instantiation 
+    of their original datatype ('0' for ints, '{}' for dicts, etc.). 
+    '''
+    with open('secrets/secret_settings.json') as f:
+        secrets = json.load(f)
+    for key in secrets:
+        t = type(secrets[key])
+        secrets[key] = t.__new__(t)
+    with open('secrets/dummy_secret_settings.json', 'wb') as f:
+        json.dump(secrets, f, indent=4)
 
 if __name__ == '__main__':
-	generate_dummy_secret_settings()
+    generate_dummy_secret_settings()
