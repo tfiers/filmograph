92eda74471215e0a3c3b9b777442d71873578aaf
tfiers <tomas.fiers@gmail.com>
2016-02-04 14:38:05 +0100

    Improved secret settings system and documentation.

--- secrets/README.md
+++ secrets/README.md
@@ -1,5 +1,14 @@
-### Usage ###
-Fill in your own secret values in `dummy_secrets.json` and rename the 
-file to `secrets.json`.
+### Purpose ###
 
-(See `settings.py` in the root directory.)
+This folder should contain a gitignored file called `secrets.json`, 
+which provides settings that are not suitable for (public) version 
+control, because they are either secret keys or settings particular 
+to a developer or production machine.
+
+`dummy_secrets.json` shows how this `secrets.json` file should be 
+structured and which key-value pairs it should contain. The dummy 
+file can be generated from a real `secrets.json` file by running 
+`settings.py` in the root directory.
+
+The secret settings are made available throughout the app through 
+`settings.py`.
--- settings.py
+++ settings.py
@@ -8,33 +8,40 @@ Usage:
 
 This includes access to secret settings, which should be saved in the 
 directory 'secrets', in a gitignored file named 'secrets.json'. The 
-file 'dummy_secrets.json' shows which secret keys should be defined.
+file 'dummy_secrets.json' shows how this file should be structured and
+which secret keys should be defined. See 'secrets/README.md'.
 
 Running this file on its own will update 'dummy_secrets.json' to 
 reflect the current state of the real 'secrets.json' file.
 """
 
 import json
+from collections import OrderedDict
 
 settings = {}
 with open('secrets/secrets.json') as f:
-    settings.update(json.load(f))
+    # We use an OrderedDict to preserve the logical ordering 
+    # ("key", "value", "description").
+    secrets = json.load(f, object_pairs_hook=OrderedDict)
+for secret in secrets:
+    settings[secret['key']] = secret['value']
 
 # -------------------------------------------------------------------
 
 def generate_dummy_secrets():
     """ Based on the real 'secrets/secrets.json' file, creates a dummy 
-    version of this file that shows which key-value pairs should be 
-    defined. The keys are preserved and the secret values are replaced 
-    by a default instantiation of their original datatype ('0' for 
-    ints, '{}' for dicts, etc.).
+    version of this file that shows how it should be structured and 
+    which key-value pairs should be defined. The keys and the 
+    descriptions are preserved, and the secret values are replaced by 
+    a default instantiation of their original datatype ('0' for ints, 
+    '{}' for dicts, etc.).
     """
     with open('secrets/secrets.json') as f:
-        secrets = json.load(f)
-    for key in secrets:
-        t = type(secrets[key])
-        secrets[key] = t.__new__(t)
-    with open('secrets/dummy_secrets.json', 'wb') as f:
+        secrets = json.load(f, object_pairs_hook=OrderedDict)
+    for secret in secrets:
+        t = type(secret['value'])
+        secret['value'] = t.__new__(t)
+    with open('secrets/dummy_secrets.json', 'w') as f:
         json.dump(secrets, f, indent=4)
 
 if __name__ == '__main__':
