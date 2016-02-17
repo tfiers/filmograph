37729659819172acbd8dd24a1de857a9f186befc
tfiers <tomas.fiers@gmail.com>
2016-02-17 11:31:44 +0100

    Refactored search() in webapp.py.

--- webapp.py
+++ webapp.py
@@ -11,13 +11,15 @@ from flask import Flask, request, render_template
 import themoviedb
 import google_images
 
-# Create the Flask WSGI application, our central webapp object.
-app = Flask('filmograph')
 
-
-@app.route('/')
-def search():
-    query = request.args.get('q')
+def get_cast_filmographies_with_images(query, num_cast_members=4,
+                                       num_screen_items=4,
+                                       num_images=4):
+    """ Combines the cast filmographies of the screen item best match-
+    ing the given query with images of the actors in their roles. Lim-
+    its the number of cast members, screen items per cast member, and
+    images per screen item to the supplied numbers.
+    """
     if query is None:
         screen_item_title = None
         cast_filmographies = None
@@ -28,16 +30,19 @@ def search():
         # with open('the_martian_cast_filmographies.json') as f:
         #     cast_filmographies = json.load(f)
         title_key = 'title' \
-                if screen_item['media_type'] == 'movie' else 'name'
+            if screen_item['media_type'] == 'movie'\
+            else 'name'
         screen_item_title = screen_item[title_key]
-        cast_filmographies = cast_filmographies[:4]
+        cast_filmographies = cast_filmographies[:num_cast_members]
         for cast_entry in cast_filmographies:
-            cast_entry['filmography'] = cast_entry['filmography'][:4]
+            cast_entry['filmography'] = \
+                        cast_entry['filmography'][:num_screen_items]
             cast_entry['role']['images_metadata'] = \
                 google_images.get_search_results_metadata(
                                     screen_item_title,
                                     cast_entry['role']['name'],
-                                    cast_entry['role']['character'])[:4]
+                                    cast_entry['role']['character']
+                                )[:num_images]
             for credit in cast_entry['filmography']:
                 credit_title_key = 'title' \
                         if credit['media_type'] == 'movie' else 'name'
@@ -45,7 +50,22 @@ def search():
                     google_images.get_search_results_metadata(
                                         credit[credit_title_key],
                                         cast_entry['role']['name'],
-                                        credit['character'])[:4]
+                                        credit['character']
+                                    )[:num_images]
+    return screen_item_title, cast_filmographies
+
+
+# --------------------------------------------------------------------
+
+# Create the Flask WSGI application, our central webapp object.
+app = Flask('filmograph')
+
+
+@app.route('/')
+def search():
+    query = request.args.get('q')
+    screen_item_title, cast_filmographies = \
+        get_cast_filmographies_with_images(query)
     return render_template('screen_item.html',
                            screen_item_title=screen_item_title,
                            cast_filmographies=cast_filmographies)
